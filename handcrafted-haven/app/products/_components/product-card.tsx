f9324fb5d44296d818f102b991dc50ce3ec34ac5
Daijir <dazhilangxianggen@gmail.com>
2025-12-04 10:10:03 +0900

    modified product-card

---
+++ handcrafted-haven/app/products/_components/product-card.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import type { Product } from "../../../lib/definitions";
+
+type ProductCardProps = {
+  product: Product;
+};
+
+export const ProductCard = ({ product }: ProductCardProps) => {
+  return (
+    <div className="group relative">
+      <div className="w-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={300}
+          height={300}
+          className="w-full h-full object-center object-cover lg:w-full lg:h-full"
+        />
+      </div>
+      <div className="mt-4 flex justify-between">
+        <div>
+          <h3 className="text-sm text-gray-700">
+            <a href={`/products/${product.id}`}>
+              <span aria-hidden="true" className="absolute inset-0" />
+              {product.name}
+            </a>
+          </h3>
+          <p className="mt-1 text-sm text-gray-500">{product.category}</p>
+        </div>
+        <p className="text-sm font-medium text-gray-900">${product.price}</p>
+      </div>
+    </div>
+  );
+};
